cfd17cf638917d92decf9afcb0bfb859040ab0d9
Oliver Drobnik <oliver@cocoanetics.com>
2011-07-05 11:06:13 +0000

    Initial Project

---
+++ DTRichTextEditor/DTRichTextEditorViewController.m
@@ -0,0 +1,64 @@
+//
+//  DTRichTextEditorViewController.m
+//  DTRichTextEditor
+//
+//  Created by Oliver Drobnik on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DTRichTextEditorViewController.h"
+
+@implementation DTRichTextEditorViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	[super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+	[super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+	    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+	} else {
+	    return YES;
+	}
+}
+
+@end
---
+++ DTRichTextEditor/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  DTRichTextEditor
+//
+//  Created by Oliver Drobnik on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTRichTextEditorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	int retVal = 0;
+	@autoreleasepool {
+	    retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([DTRichTextEditorAppDelegate class]));
+	}
+	return retVal;
+}
