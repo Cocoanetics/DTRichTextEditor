f0a41d2fa2555141a884776e7af385cd2fd40e30
Oliver Drobnik <oliver@cocoanetics.com>
2013-04-09 15:52:06 +0200

    Moved constants to DTRichTextEditorConstants

---
+++ Core/Source/DTRichTextEditorConstants.m
@@ -0,0 +1,12 @@
+//
+//  DTRichTextEditorConstants.m
+//  DTRichTextEditor
+//
+//  Created by Oliver Drobnik on 09.04.13.
+//  Copyright (c) 2013 Cocoanetics. All rights reserved.
+//
+
+#import "DTRichTextEditorConstants.h"
+
+NSString * const DTParagraphSpacingOverriddenByListAttribute = @"DTParagraphSpacingOverriddenByList";
+NSString * const DTSelectionMarkerAttribute = @"DTSelectionMarker";
