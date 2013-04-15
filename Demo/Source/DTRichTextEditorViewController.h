e667724c4fddc75d719044c932a495f8fd961248
Daniel Phillips <djp_phillips@me.com>
2013-04-15 09:55:53 -0300

    Omitted @class directory to allow reference to controller

--- Demo/Source/DTRichTextEditorViewController.h
+++ Demo/Source/DTRichTextEditorViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "DTRichTextEditorView.h"
 
-@class DTRichTextEditorTestState;
+@class DTRichTextEditorTestState, DTRichTextEditorTestStateController;
 
 @interface DTRichTextEditorViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPopoverControllerDelegate, DTAttributedTextContentViewDelegate, DTRichTextEditorViewDelegate> {
 
